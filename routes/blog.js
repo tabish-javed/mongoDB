15e551d65a03ce78420c0595f2563874c71ff320
TABISH <tabishjaved@hotmail.com>
2023-01-29 11:33:47 +0530

    retriev post list and post detail finished

--- routes/blog.js
+++ routes/blog.js
@@ -11,9 +11,14 @@ router.get('/', function (req, res) {
     res.redirect('/posts');
 });
 
-router.get('/posts', function (req, res) {
-    res.render('posts-list');
-});
+router.get('/posts', async function (req, res) {
+    const posts = await db
+        .getDb().collection("posts")
+        .find({})
+        .project({ title: 1, summary: 1, "author.name": 1 })
+        .toArray()
+    res.render('posts-list', { posts: posts })
+})
 
 router.get('/new-post', async function (req, res) {
     const authors = await db.getDb().collection("authors").find().toArray()
@@ -35,9 +40,30 @@ router.post('/posts', async function (req, res) {
             email: author.email
         }
     }
-    const result = await db.getDb().collection("posts").insertOne(newPost)
-    // console.log(result)
+    await db.getDb().collection("posts").insertOne(newPost)
     res.redirect('/posts')
+})
+
+router.get("/posts/:id", async function (req, res) {
+    const postID = req.params.id
+    const post = await db
+        .getDb()
+        .collection("posts")
+        .findOne({ _id: new ObjectId(postID) }, { summary: 0 })
+    if (!post) {
+        return res.status(404).render("404")
+    }
+
+    post.humanReadableDate = post.date.toLocaleDateString("en-US", {
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric"
+    })
+
+    post.date = post.date.toISOString()
+
+    res.render("post-detail", {post: post})
 })
 
 module.exports = router;
